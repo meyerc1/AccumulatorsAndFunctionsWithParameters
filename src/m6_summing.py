99be5c0e67b5240dca93b5c837b66a97b1104855
meyerc1 <meyerc1@rose-hulman.edu>
2017-11-30 13:52:18 -0500

    All TODO's are finished with proper code.

--- src/m6_summing.py
+++ src/m6_summing.py
@@ -4,9 +4,10 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Carson Meyer.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
+import math as mt
 
 def main():
     """ Calls the   TEST   functions in this module. """
@@ -17,7 +18,7 @@ def main():
 def run_test_sum_cosines():
     """ Tests the   sum_cosines   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_cosines  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -29,6 +30,32 @@ def run_test_sum_cosines():
     print('Testing the   sum_cosines   function:')
     print('--------------------------------------------------')
 
+    #Test1
+    print('Test 1')
+    expected1 = 1.1241554693209974
+    answer1 = sum_cosines(3)
+    print('expected =', expected1)
+    print('answer =', answer1)
+    if (expected1 == answer1):
+        print('Sweet Sassy Molassy!')
+
+    #Test2
+    print('Test 2')
+    expected2 = -0.5194806481430599
+    answer2 = sum_cosines(5)
+    print('expected =', expected2)
+    print('answer =', answer2)
+    if (expected2 == answer2):
+        print('Sweet Sassy Molassy!')
+
+    #Test3
+    print('Test 3')
+    expected3 = 1.478254078313837
+    answer3 = sum_cosines(8)
+    print('expected =', expected3)
+    print('answer =', answer3)
+    if (expected3 == answer3):
+        print('Sweet Sassy Molassy!')
 
 def sum_cosines(n):
     """
@@ -40,8 +67,13 @@ def sum_cosines(n):
       If n is 3, this function returns
         cos(0) + cos(1) + cos(2) + cos(3)   which is about 0.13416.
     """
+    cos_val = 0
+    for k in range(n):
+        cos_val = cos_val + mt.cos(k)
+
+    return cos_val
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
@@ -53,7 +85,7 @@ def sum_cosines(n):
 def run_test_sum_square_roots():
     """ Tests the   sum_square_roots   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_square_roots  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -65,6 +97,32 @@ def run_test_sum_square_roots():
     print('Testing the   sum_square_roots   function:')
     print('--------------------------------------------------')
 
+    #Test1
+    print('Test 1')
+    expected1 = 15.811388300841898
+    answer1 = sum_square_roots(5)
+    print('expected =', expected1)
+    print('answer =', answer1)
+    if (expected1 == answer1):
+        print('Sweet Sassy Molassy!')
+
+    #Test2
+    print('Test 2')
+    expected2 = 20.784609690826525
+    answer2 = sum_square_roots(6)
+    print('expected =', expected2)
+    print('answer =', answer2)
+    if (expected2 == answer2):
+        print('Sweet Sassy Molassy!')
+
+    #Test3
+    print('Test 3')
+    expected3 = 32.0
+    answer3 = sum_square_roots(8)
+    print('expected =', expected3)
+    print('answer =', answer3)
+    if (expected3 == answer3):
+        print('Sweet Sassy Molassy!')
 
 def sum_square_roots(n):
     """
@@ -79,8 +137,14 @@ def sum_square_roots(n):
          sqrt(2) + sqrt(4) + sqrt(6) + sqrt(8) + sqrt(10),
       which is about 11.854408.
     """
+    total = 0
+    for k in range(n):
+        total = total + mt.sqrt(n * 2)
+
+    return total
+
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
