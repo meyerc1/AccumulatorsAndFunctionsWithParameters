57983ecce440dce2c77c2fabe9164da53d89ee4d
meyerc1 <meyerc1@rose-hulman.edu>
2017-12-04 09:15:19 -0500

    All TODO's are finished with proper code.

--- src/m9_summing_again.py
+++ src/m9_summing_again.py
@@ -10,14 +10,14 @@ Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_sum_powers()
+    #run_test_sum_powers()
     run_test_sum_powers_in_range()
 
 
 def run_test_sum_powers():
     """ Tests the   sum_powers   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_powers  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -28,6 +28,24 @@ def run_test_sum_powers():
     print('--------------------------------------------------')
     print('Testing the   sum_powers   function:')
     print('--------------------------------------------------')
+
+    #Test 1
+    print('--------------------------------------------')
+    print('Test 1: Sum of Powers')
+    print('--------------------------------------------')
+    sum_powers(5, -0.3)
+
+    #Test 2
+    print('--------------------------------------------')
+    print('Test 2: Sum of Powers')
+    print('--------------------------------------------')
+    sum_powers(100, 0.1)
+
+    #Test 3
+    print('--------------------------------------------')
+    print('Test 3: Sum of Powers')
+    print('--------------------------------------------')
+    sum_powers(20, 5)
 
 
 def sum_powers(n, p):
@@ -43,18 +61,26 @@ def sum_powers(n, p):
       -- sum_powers(100, 0.1) returns about 144.45655
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
 
+    number = 0
+    for k in range(n):
+        value = ((k + 1) ** p)
+        number = number + value
+        print(number)
+
+    return number
+
 
 def run_test_sum_powers_in_range():
     """ Tests the   sum_powers_in_range   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_powers_in_range  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -65,6 +91,24 @@ def run_test_sum_powers_in_range():
     print('--------------------------------------------------')
     print('Testing the   sum_powers_in_range   function:')
     print('--------------------------------------------------')
+
+    #Test 1
+    print('--------------------------------------------')
+    print('Test 1: Sum of Powers in Range')
+    print('--------------------------------------------')
+    sum_powers_in_range(3, 100, 0.1)
+
+    #Test 2
+    print('--------------------------------------------')
+    print('Test 2: Sum of Powers in Range')
+    print('--------------------------------------------')
+    sum_powers_in_range(5, 15, 2)
+
+    #Test 3
+    print('--------------------------------------------')
+    print('Test 3: Sum of Powers in Range')
+    print('--------------------------------------------')
+    sum_powers_in_range(10, 20, 0.5)
 
 
 def sum_powers_in_range(m, n, p):
@@ -80,13 +124,18 @@ def sum_powers_in_range(m, n, p):
       -- sum_powers_in_range(3, 100, 0.1) returns about 142.384776
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers_in_range  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
+    number = 0
+    for k in range(n):
+        number = ((m + k) ** p) + number
+        print(number)
 
+    return number
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
